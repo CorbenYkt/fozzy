ae263ee3c24faa829a87904516e87a2c44fed825
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2022-12-12 09:31:13 +0600

    new photos

--- src/components/myfamily/About.js
+++ src/components/myfamily/About.js
@@ -22,22 +22,30 @@ export default function About() {
 
             </Grid>
             <Grid item xs={8}>
-                <Item><Carousel showThumbs={false} dynamicHeight={false} infiniteLoop={true} autoPlay={true} stopOnHover={true}>
+                <Item><Carousel dynamicHeight={true} showThumbs={false}  infiniteLoop={true} autoPlay={true} stopOnHover={true}>
                     <div>
-                        <img src={require('./photo/1.jpg')} />
-                        <p className="legend">This is my Bro, his name is Gosha. He is in 6th grade of school</p>
+                        <img src={require('./photo/4.jpg')} />
+                        <p className="legend">This is my Family, - Tanya, Gosha (Bro), and Dima</p>
                     </div>
                     <div>
                         <img src={require('./photo/2.jpg')} />
-                        <p className="legend">He, like me, likes nature</p>
+                        <p className="legend">Gosha, is in 6th grade of school and he, like me, likes nature</p>
                     </div>
                     <div>
                         <img src={require('./photo/3.jpg')} />
                         <p className="legend">And also Mom and Dad</p>
                     </div>
                     <div>
+                        <img src={require('./photo/5.jpg')} />
+                        <p className="legend">Me and my Jedy's cloak</p>
+                    </div>
+                    <div>
+                        <img src={require('./photo/6.jpg')} />
+                        <p className="legend">Me and my Bro Gosha</p>
+                    </div>
+                    <div>
                         <img src={require('./photo/4.jpg')} />
-                        <p className="legend">They are all together on one shot</p>
+                        <p className="legend">Walking around</p>
                     </div>
 
                 </Carousel>
