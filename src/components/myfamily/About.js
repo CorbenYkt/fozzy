9d1ccdde38e59af37753ba18c67992f811af38a9
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-01-06 08:46:01 +1300

    new images

--- src/components/myfamily/About.js
+++ src/components/myfamily/About.js
@@ -8,56 +8,65 @@ var images = [
         sub: 'This is my Family, - Tanya, Gosha (Bro), and Dima'
     },
     {
-        src: require('./photo/3.jpg'),
-        sub: 'And also Mom and Dad'
-    },
-    {
-        src: require('./photo/2.jpg'),
-        sub: 'Gosha, is in 6th grade of school and he, like me, likes nature'
-    },
-    {
-        src: require('./photo/5.jpg'),
-        sub: 'Me and my Jedy\'s cloak',
-    },
-    {
         src: require('./photo/6.jpg'),
         sub: 'Me and my Bro Gosha'
     },
     {
-        src: require('./photo/7.jpg'),
-        sub: 'Walking around'
+        src: require('./photo/17.jpg'),
+        sub: 'My family already in New Zealand'
     },
     {
-        src: require('./photo/8.jpg'),
-        sub: 'I like to take a nap'
-    },
-    {
-        src: require('./photo/9.jpg'),
-        sub: 'I like to travel'
-    },
-    {
-        src: require('./photo/10.jpg'),
-        sub: 'My usual days in winter'
-    },
-    {
-        src: require('./photo/11.jpg'),
-        sub: 'Again put a big luminous tree'
-    },
-    {
-        src: require('./photo/12.jpg'),
-        sub: 'Road for a walk'
-    },
-    {
-        src: require('./photo/13.jpg'),
-        sub: 'After visiting Ayusai waterfalls'
+        src: require('./photo/16.jpg'),
+        sub: 'I\'m in South Korea in quarantine before entering New Zealand'
     },
     {
         src: require('./photo/14.jpg'),
         sub: 'My brother and bricks'
     },
     {
+        src: require('./photo/13.jpg'),
+        sub: 'After visiting Ayusai waterfalls'
+    },
+    {
         src: require('./photo/15.jpg'),
         sub: 'Mountains... mountains'
+    },
+    {
+        src: require('./photo/12.jpg'),
+        sub: 'Road for a walk'
+    },
+    {
+        src: require('./photo/11.jpg'),
+        sub: 'Again put a big luminous tree'
+    },
+    {
+        src: require('./photo/10.jpg'),
+        sub: 'My usual days in winter'
+    },
+    {
+        src: require('./photo/9.jpg'),
+        sub: 'I like to travel'
+    },
+    {
+        src: require('./photo/8.jpg'),
+        sub: 'I like to take a nap'
+    },
+    {
+        src: require('./photo/7.jpg'),
+        sub: 'Walking around'
+    },
+    
+    {
+        src: require('./photo/5.jpg'),
+        sub: 'Me and my Jedy\'s cloak',
+    },
+    {
+        src: require('./photo/3.jpg'),
+        sub: 'And also Mom and Dad'
+    },
+    {
+        src: require('./photo/2.jpg'),
+        sub: 'Gosha, is in 6th grade of school and he, like me, likes nature'
     }
 ]
 
