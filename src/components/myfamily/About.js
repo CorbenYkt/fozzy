034941fe4d15ec2b9cc4e050e74c46cfe87a7a8f
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-01-16 10:26:42 +1300

    new gallery plugin used

--- src/components/myfamily/About.js
+++ src/components/myfamily/About.js
@@ -1,72 +1,90 @@
 import React, { useState, useCallback } from "react";
 import Lightroom from 'react-lightbox-gallery'
 import { Box, Grid } from "@mui/material";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 var images = [
     {
-        src: require('./photo/4.jpg'),
-        sub: 'This is my Family, - Tanya, Gosha (Bro), and Dima'
+        original: require('./photo/4.jpg'),
+        thumbnail: require('./photo/4l.jpg'),
+        description: 'This is my Family, - Tanya, Gosha (Bro), and Dima'
     },
     {
-        src: require('./photo/6.jpg'),
-        sub: 'Me and my Bro Gosha'
+        original: require('./photo/6.jpg'),
+        thumbnail: require('./photo/6l.jpg'),
+        description: 'Me and my Bro Gosha'
     },
     {
-        src: require('./photo/17.jpg'),
-        sub: 'My family already in New Zealand'
+        original: require('./photo/17.jpg'),
+        thumbnail: require('./photo/17l.jpg'),
+        description: 'My family already in New Zealand'
     },
     {
-        src: require('./photo/16.jpg'),
-        sub: 'I\'m in South Korea in quarantine before entering New Zealand'
+        original: require('./photo/16.jpg'),
+        thumbnail: require('./photo/16l.jpg'),
+        description: 'I\'m in South Korea in quarantine before entering New Zealand'
     },
     {
-        src: require('./photo/14.jpg'),
-        sub: 'My brother and bricks'
+        original: require('./photo/14.jpg'),
+        thumbnail: require('./photo/14l.jpg'),
+        description: 'My brother and bricks'
     },
     {
-        src: require('./photo/13.jpg'),
-        sub: 'After visiting Ayusai waterfalls'
+        original: require('./photo/13.jpg'),
+        thumbnail: require('./photo/13l.jpg'),
+        description: 'After visiting Ayusai waterfalls'
     },
     {
-        src: require('./photo/15.jpg'),
-        sub: 'Mountains... mountains'
+        original: require('./photo/15.jpg'),
+        thumbnail: require('./photo/15l.jpg'),
+        description: 'Mountains... mountains'
     },
     {
-        src: require('./photo/12.jpg'),
-        sub: 'Road for a walk'
+        original: require('./photo/12.jpg'),
+        thumbnail: require('./photo/12l.jpg'),
+        description: 'Road for a walk'
     },
     {
-        src: require('./photo/11.jpg'),
-        sub: 'Again put a big luminous tree'
+        original: require('./photo/11.jpg'),
+        thumbnail: require('./photo/11l.jpg'),
+        description: 'Again put a big luminous tree'
     },
     {
-        src: require('./photo/10.jpg'),
-        sub: 'My usual days in winter'
+        original: require('./photo/10.jpg'),
+        thumbnail: require('./photo/10l.jpg'),
+        description: 'My usual days in winter'
     },
     {
-        src: require('./photo/9.jpg'),
-        sub: 'I like to travel'
+        original: require('./photo/9.jpg'),
+        thumbnail: require('./photo/9l.jpg'),
+        description: 'I like to travel'
     },
     {
-        src: require('./photo/8.jpg'),
-        sub: 'I like to take a nap'
+        original: require('./photo/8.jpg'),
+        thumbnail: require('./photo/8l.jpg'),
+        description: 'I like to take a nap'
     },
     {
-        src: require('./photo/7.jpg'),
-        sub: 'Walking around'
+        original: require('./photo/7.jpg'),
+        thumbnail: require('./photo/7l.jpg'),
+        description: 'Walking around'
     },
-    
+
     {
-        src: require('./photo/5.jpg'),
-        sub: 'Me and my Jedy\'s cloak',
-    },
-    {
-        src: require('./photo/3.jpg'),
-        sub: 'And also Mom and Dad'
+        original: require('./photo/5.jpg'),
+        thumbnail: require('./photo/5l.jpg'),
+        description: 'Me and my Jedy\'s cloak',
     },
     {
-        src: require('./photo/2.jpg'),
-        sub: 'Gosha, is in 6th grade of school and he, like me, likes nature'
+        original: require('./photo/3.jpg'),
+        thumbnail: require('./photo/3l.jpg'),
+        description: 'And also Mom and Dad'
+    },
+    {
+        original: require('./photo/2.jpg'),
+        thumbnail: require('./photo/2l.jpg'),
+        description: 'Gosha, is in 6th grade of school and he, like me, likes nature'
     }
 ]
 
@@ -81,14 +99,21 @@ var settings = {
 
 export default function About() {
     const newimages = images.reverse()
-
     return (
         <Box component={'main'} display={'flex'} flexDirection={{ xs: 'column', md: 'row' }} alignItems={'center'}
             justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
             <Grid container justifyContent="center">
                 <Grid item xs={8}>
                     <br></br>
-                    <Lightroom images={newimages} settings={settings} />
+                    <ImageGallery items={images}
+                        showNav={false}
+                        useBrowserFullscreen={false}
+                        showPlayButton={true}
+                        showIndex={true}
+                        disableSwipe={false}
+                        showThumbnails={false}
+                        lazyLoad={true}></ImageGallery>
+
                 </Grid>
             </Grid>
         </Box>
